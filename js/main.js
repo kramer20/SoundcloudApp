c511397ae774f4d0782d4a72b4bf79fb1dde741f
Malorie Kramer <maloriekramer@Malorie-Kramers-MacBook-Pro.local>
2016-06-20 17:15:24 -0400

    play music

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
 $( document ).ready(function() {
    
+$(".searchArtist").focus();
 
 $("form").on("submit",function(e){
 	e.preventDefault();
 
-	var search = $(".searchArtist").val();
-
-	var data = {
-		searchArtist:search,
-	};
+		var search = $(".searchArtist").val();
+		
+		$(".searchArtist").val("");
 
 	$.ajax({
 	url: 'http://api.soundcloud.com/tracks?client_id=03e4633e2d85874a921380e47cac705d&q='+search, 
 	type:"GET",
 	success: function(result){
 
-	var playlist = result;
+		var playlist = result;
 
-	playlist.forEach(function (music) {
-		$(".playlistRow").append('<div class="col-md-3 bottomRow"><div class="imageBox"><img class="coverPhoto" src='+music.artwork_url+'></div><p class="songTitle">'+music.title+'</div>');
-	});
-}
+		playlist.forEach(function (music) {
+			$(".playlistRow").append('<div class="col-md-3 bottomRow"><div class="imageBox"><img data-song="'+music.id+'" class="coverPhoto" src='+music.artwork_url+'></div><p class="songTitle">'+music.title+'</div>');
+		});
+
+
+	$("img").on('click',function(){
+		var imgClick = $(this).data("song");
+		var songSource = "http://api.soundcloud.com/tracks/"+imgClick+"/stream?client_id=03e4633e2d85874a921380e47cac705d"	
+			$("audio").attr("src",songSource);
+			//alert(imgClick);
+
+		})
+
+	}
+
 
 });
 
